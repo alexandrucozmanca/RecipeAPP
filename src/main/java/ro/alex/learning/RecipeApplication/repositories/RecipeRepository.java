1e9d8951d8a671c563772885fcb428529711daf9
alexandru.cozmanca <alexandru.cozmanca@gmail.com>
2018-10-12 13:35:30 +0300

    Query Method - findByID

--- src/main/java/ro/alex/learning/RecipeApplication/repositories/RecipeRepository.java
+++ src/main/java/ro/alex/learning/RecipeApplication/repositories/RecipeRepository.java
@@ -6,5 +6,5 @@ import ro.alex.learning.RecipeApplication.domain.Recipe;
 import java.util.Optional;
 
 public interface RecipeRepository extends CrudRepository<Recipe,Long> {
-    Optional<Recipe> findByDescription();
+    Optional<Recipe> findByDescription(String description);
 }
