53d5a30b60a7068de8945850cda83d8b354ef25c
alexandru.cozmanca <alexandru.cozmanca@gmail.com>
2018-10-12 13:27:03 +0300

    Query Method - findByID

--- src/main/java/ro/alex/learning/RecipeApplication/repositories/UnitOfMeasureRepository.java
+++ src/main/java/ro/alex/learning/RecipeApplication/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package ro.alex.learning.RecipeApplication.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ro.alex.learning.RecipeApplication.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
